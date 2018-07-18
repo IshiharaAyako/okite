3def05d3d925ff8aa61da143c03cc90e5ecf13fc
石原 理子 <a.ishihara@112-283.local>
2018-07-18 10:47:12 +0900

    teratailの回答追加

--- okite/ViewController.swift
+++ okite/ViewController.swift
@@ -10,14 +10,15 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    //追加-
+    var speechText: String = ""
+    //----
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         timeLabel.text = getTimeNow()
         _ = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(timeUpdate), userInfo: nil, repeats: true)
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
     }
     
     var audioPlayer:AVAudioPlayer!
@@ -39,7 +40,7 @@ class ViewController: UIViewController {
     @IBAction func buttonFunc(_ sender: AnyObject) {
         setTime = tmpTime
         print("setTime:" + setTime)
-        setsAlert()
+        setAlert()
     }
     
     func getTimeNow()-> String {
@@ -101,15 +102,21 @@ class ViewController: UIViewController {
     }
     
     func voice() {
-        /*SpeechSynthesizerクラス*/
+        //        /*SpeechSynthesizerクラス*/
+        //        let talker = AVSpeechSynthesizer()
+        //        // 話す内容をセット
+        //        let utterance = AVSpeechUtterance(string: "今日の予定は\(myTextField)です。さぁ起きましょう")
+        //        //        let plan: String = "あああああああ"
+        //        //        let utterance = AVSpeechUtterance(string: "今日の予定は\(plan)です。さぁ起きましょう")
+        //        // 言語を日本に設定
+        //        utterance.voice = AVSpeechSynthesisVoice(language: "ja-JP")
+        //        // 実行
+        //        talker.speak(utterance)
+        
+        //追加
         let talker = AVSpeechSynthesizer()
-        // 話す内容をセット
-        let utterance = AVSpeechUtterance(string: "今日の予定はです。〇〇さぁ起きましょう")
-        //        let plan: String = "あああああああ"
-        //        let utterance = AVSpeechUtterance(string: "今日の予定は\(plan)です。さぁ起きましょう")
-        // 言語を日本に設定
+        let utterance = AVSpeechUtterance(string: "今日の予定は\(speechText)です。")
         utterance.voice = AVSpeechSynthesisVoice(language: "ja-JP")
-        // 実行
         talker.speak(utterance)
     }
     
@@ -118,16 +125,13 @@ class ViewController: UIViewController {
         voice()
     }
     
-    //=======================================================================
-    
-    func setsAlert(){
+    func setAlert(){
         let setAlert: UIAlertController = UIAlertController(title: "予定を追加しますか？", message: "", preferredStyle:  UIAlertControllerStyle.alert)
         
         let defaultAction: UIAlertAction = UIAlertAction(title: "する", style: UIAlertActionStyle.default, handler:{
             (action: UIAlertAction!) -> Void in
             //self.planAlert()
             self.test()
-            //print("pushed するbutton")
         })
         let cancelAction: UIAlertAction = UIAlertAction(title: "しない", style: UIAlertActionStyle.cancel, handler:{
             (action: UIAlertAction!) -> Void in
@@ -140,57 +144,38 @@ class ViewController: UIViewController {
         present(setAlert, animated: true, completion: nil)
     }
     
-    //    func planAlert() {
-    //        let questionAlert: UIAlertController = UIAlertController(title: "今日の予定は？", message: "", preferredStyle:  UIAlertControllerStyle.alert)
-    //        let okAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:{
-    //            (action: UIAlertAction!) -> Void in
-    //            print("pushed OKbutton")
-    //        })
-    //
-    //        //文字
-    //        questionAlert.addTextField(configurationHandler: {(textField: UITextField!) -> Void in
-    //                        // NotificationCenterを生成.
-    //                        let myNotificationCenter = NotificationCenter.default
-    //                        // textFieldに変更があればchangeTextFieldメソッドに通知.
-    //                        myNotificationCenter.addObserver(self, selector: Selector(("changeTextField:")), name: NSNotification.Name.UITextFieldTextDidChange, object: nil)
-    //
-    //                        self.myTextField = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
-    //                        // 表示する文字を代入する.
-    //                        self.myTextField.text = "Hello Swift!!"
-    //                        // Delegateを設定する.
-    //                        self.myTextField.delegate = self as? UITextFieldDelegate
-    //                        // 枠を表示する.
-    //                        self.myTextField.borderStyle = UITextBorderStyle.roundedRect
-    //                        // UITextFieldの表示する位置を設定する.
-    //                        self.myTextField.layer.position = CGPoint(x:self.view.bounds.width/2,y:100);
-    //                        // Viewに追加する.
-    //                        self.view.addSubview(self.myTextField)
-    //
-    //            textField.placeholder = "テキスト"
-    //
-    //        })
-    //
-    //        //UIAlertControllerにActionを追加
-    //        questionAlert.addAction(okAction)
-    //        //Alertを表示
-    //        present(questionAlert, animated: true, completion: nil)
-    //    }
-    
     func test() {
         let questionAlert = UIAlertController(title: "今日の予定は？", message: "", preferredStyle: .alert)
         questionAlert.addTextField(configurationHandler: { (textField:UITextField) -> Void in
             textField.placeholder = "placeholder"
         })
-        questionAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action:UIAlertAction) -> Void in
-            let textField = questionAlert.textFields![0] as UITextField
-            print("Text field: \(String(describing: textField.text))")
-            
-        }))
-        questionAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action:UIAlertAction) -> Void in
-            print("Text field: cancel")
-        }))
-        self.present(questionAlert, animated: true, completion: nil)
         
+        //        questionAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action:UIAlertAction) -> Void in
+        //        let textField = questionAlert.textFields![0] as UITextField
+        //        print("Text field: \(String(describing: textField.text))")
+        //        }))
+        //
+        //        questionAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action:UIAlertAction) -> Void in
+        //        print("Text field: cancel")
+        //        }))
+        //        self.present(questionAlert, animated: true, completion: nil)
+        
+        //追加
+        let speechAction = UIAlertAction(title: "Speech", style: .default, handler: { (_) -> Void in
+            // TextFieldから値を取得
+            if let text = questionAlert.textFields?.first?.text {
+                self.speechText = text
+                self.voice()
+            }
+        })
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        questionAlert.addAction(speechAction)
+        questionAlert.addAction(cancel)
+        questionAlert.addTextField { (textField) in }
+        present(questionAlert, animated: true, completion: nil)
     }
-    
 }
+
+
